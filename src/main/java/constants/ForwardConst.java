0e66137e5b98587767f926182f1add451324ea0e
uchidatatsuya <uchidatatsuya@172.20.10.2>
2022-06-17 20:49:00 +0900

    FrontController作成

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -1,17 +1,17 @@
 package constants;
 
-// リクエストパラメーターの変数名、変数値、jspファイルの名前等画面遷移に関わる値を定義するEnumクラス
+ // リクエストパラメーターの変数名、変数値、jspファイルの名前等画面遷移に関わる値を定義するEnumクラス
 
 public enum ForwardConst {
 
-    // action
+    //action
     ACT("action"),
     ACT_TOP("Top"),
     ACT_EMP("Employee"),
     ACT_REP("Report"),
     ACT_AUTH("Auth"),
 
-    // command
+    //command
     CMD("command"),
     CMD_NONE(""),
     CMD_INDEX("index"),
@@ -25,7 +25,7 @@ public enum ForwardConst {
     CMD_UPDATE("update"),
     CMD_DESTROY("destroy"),
 
-    // jsp
+    //jsp
     FW_ERR_UNKNOWN("error/unknown"),
     FW_TOP_INDEX("topPage/index"),
     FW_LOGIN("login/login"),
@@ -34,21 +34,20 @@ public enum ForwardConst {
     FW_EMP_NEW("employees/new"),
     FW_EMP_EDIT("employees/edit"),
     FW_REP_INDEX("reports/index"),
-    FW_REP_SHOW("repots/show"),
+    FW_REP_SHOW("reports/show"),
     FW_REP_NEW("reports/new"),
-    FW_REP_EDIT("reports/edit"),
+    FW_REP_EDIT("reports/edit");
 
-    // 文字列
+     // 文字列
     private final String text;
 
-    // コンストラクタ
+     // コンストラクタ
     private ForwardConst(final String text) {
         this.text = text;
     }
 
-    // 値（文字列）取得
+     // 値(文字列)取得
     public String getValue() {
         return this.text;
     }
-
 }
